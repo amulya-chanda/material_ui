e9bd5c46b4f4510983fceefd258f92211f60dead
amulya <amulya@divami.com>
2023-03-13 14:22:24 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Mobile } from './components/Mobile';
 import { Footer } from './components/Footer';
-//import { Mobile_contents } from "./common_components/mobile_contents";
 import {Card} from "./components/Card";
 import {Persons} from "./components/People_data";
 import './App.css';
-import Carousel from '@itseasy21/react-elastic-carousel'
+import Carousel from '@itseasy21/react-elastic-carousel';
+import {createTheme,ThemeProvider,CssBaseline} from "@mui/material";
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      },
+    });
     return(
       <>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      
+    
       <Header/>
       <Mobile/>
-      {/* <Mobile_contents/> */}
       <Card/>
       <Carousel>
       <Persons/>
@@ -20,6 +28,7 @@ function App() {
       <Persons/>
       </Carousel>
       <Footer/>
+      </ThemeProvider>
       </>
     )
 }
